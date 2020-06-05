eb8bf72f9f0b6abbf36ffbc33fc9ea30177ec4a1
Son Dieu <sdieu@Sons-MacBook-Pro.local>
2020-06-05 15:49:49 +0700

    run in local

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "graphiql/graphiql.css"
+import "./App.css"
 
-function App() {
+import React, { useState, useCallback } from 'react';
+// import GraphiQL from '../vendor/graphiql';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
+
+export default () => {
+
+  const [api, setApi] = useState("https://api.skedulo.com/graphql/graphql")
+  const [token, setToken] = useState("")
+
+  const graphQLFetcher = useCallback((graphQLParams) => {
+    return fetch(api, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(graphQLParams),
+    }).then(response => response.json());
+  }, [api, token])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="setting">
+        <input className="form-control" placeholder="API" value={api} onChange={(evt) => setApi(evt.target.value)} />
+        <input className="form-control" placeholder="Bearer Token" value={token} onChange={(evt) => setToken(evt.target.value)} />
+      </div>
+
+      <div className="main-work-space">
+        <GraphiQL fetcher={graphQLFetcher} />
+      </div>
     </div>
   );
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
