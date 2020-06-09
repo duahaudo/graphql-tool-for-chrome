cb99df9cee6bf8d3d066eaeb11181aa8faec2174
Son Dieu <sdieu@Sons-MacBook-Pro.local>
2020-06-09 11:36:08 +0700

    verify tabs already open

--- public/js/background.js
+++ public/js/background.js
@@ -1,17 +1,25 @@
 chrome.browserAction.onClicked.addListener((tab1) => {
+    chrome.tabs.query({ title: "GraphQL tools by Stiger" }, (tabs) => {
+
+    })
+
+    openNewTab();
+})
+
+const openNewTab = () => {
     // Get the current tab
-    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0]
         if (tab.url.includes("skedulo")) {
             // Execute script in the current tab
             chrome.tabs.executeScript(tab.id, { code: `localStorage['auth']` }, ([fromPageLocalStore]) => {
                 const skedApiAccessToken = JSON.parse(fromPageLocalStore).skedApiAccessToken;
-                chrome.tabs.create({ url: `index.html?` + skedApiAccessToken }, (newTab) => {
-                    // chrome.tabs.executeScript(newTab.id, { code: `localStorage.setItem('auth', "${skedApiAccessToken}")` })
+                chrome.tabs.create({ url: `index.html?` + skedApiAccessToken, active: true }, (newTab) => {
+                    chrome.tabs.executeScript(newTab.id, { code: `localStorage.setItem('auth', "${skedApiAccessToken}")` })
                 })
             });
         } else {
             chrome.tabs.create({ url: 'index.html' })
         };
     })
-})
-
+}
